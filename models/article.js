85af4cffa5ea779d507f59377c9518d56d97065f
Alexander Kozhin <a.d.kozhin@yandex.ru>
2020-11-06 16:22:58 +0300

    feat: add articleSchema

---
+++ models/article.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const { ObjectID } = require('mongodb');
+
+const articleSchema = new mongoose.Schema({
+  keyword: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  source: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+        return /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/.test(v);
+      },
+      message: 'Link is not valid',
+    },
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+        return /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/.test(v);
+      },
+      message: 'Image link is not valid',
+    },
+  },
+  owner: {
+    type: ObjectID,
+    required: [true, 'OwnerId is required'],
+  },
+});
+
+module.exports = mongoose.model('article', articleSchema);
