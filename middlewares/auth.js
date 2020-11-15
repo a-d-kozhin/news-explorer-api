7ef0f0e8f63d313b8d8543911eb38ed66fc3c841
Alexander Kozhin <a.d.kozhin@yandex.ru>
2020-11-15 21:19:07 +0300

    hotfix: JWT_SECRET issue in dev mode

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,11 +1,12 @@
 const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = require('../utils/environment');
 
 module.exports.auth = (req, res, next) => {
   const token = req.headers.authorization
     && req.headers.authorization.replace('Bearer ', '');
   let payload;
   try {
-    payload = jwt.verify(token, process.env.JWT_SECRET);
+    payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
     res.status(401).send({ message: 'Необходима авторизация' });
   }
